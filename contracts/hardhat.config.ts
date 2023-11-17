ff6bf5299fff8e5870a7ff03be3545f135c9b99a
Prashant Srivastava <prashantsrivastava1001@gmail.com>
2023-11-17 12:59:03 +0530

    not in working

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
+};
+
+export default config;
